dbe1e0daf6268020753c3d1de7d17b91a89a12c3
Guilherme <guilherme.brusque@hural.com.br>
2024-05-23 13:15:21 -0300

    Added class FinishVideoInitialization in QGCApplication that was removed in last commit

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -138,6 +138,21 @@
 
 #include "QGCMapEngine.h"
 
+class FinishVideoInitialization : public QRunnable
+{
+public:
+  FinishVideoInitialization(VideoManager* manager)
+      : _manager(manager)
+  {}
+
+  void run () {
+      _manager->_initVideo();
+  }
+
+private:
+  VideoManager* _manager;
+};
+
 
 QGCApplication* QGCApplication::_app = nullptr;
 
